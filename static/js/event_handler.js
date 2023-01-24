8003c09d995a873ae75899b4a3689d7af0f20815
AngeloPavone <pavone@gmx.us>
2023-01-23 22:55:54 -0800

    added ability to see last chat message moved js to its own file

---
+++ static/js/event_handler.js
@@ -0,0 +1,18 @@
+var socket = io.connect('http://' + document.domain + ':' + location.port);
+socket.on('connect', function(){
+    socket.emit('message_event',{
+        network_log: 'User Connected'
+    })
+        var form = $('form.chatbox').on('submit', function(e){
+            e.preventDefault()
+            let user_input = $('#message').val()
+            socket.emit('message_event',{
+                chat_log: user_input
+            })
+            $('#message').val("").focus()
+        })
+    })
+    socket.on('my response', function(msg) {
+        console.log(msg)
+        $('.chat_message').text(msg)
+})
