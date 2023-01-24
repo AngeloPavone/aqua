ff0c627c268329d90d982727b1c529a2b0fd3285
AngeloPavone <pavone@gmx.us>
2023-01-23 22:55:33 -0800

    added ability to see last chat message moved js to its own file

--- app.py
+++ app.py
@@ -2,10 +2,9 @@ from flask import Flask, render_template
 from flask_socketio import SocketIO
 
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="static")
 app.config['SECRET_KEY'] = "dontworryaboutitbruh"
 socketio = SocketIO(app)
-
 
 @app.route("/", methods=["GET", "POST"])
 def home() -> str:
@@ -15,16 +14,14 @@ def message_confirm(methods=["GET","POST"]) -> None:
     print(f"Message recieved")
 
 @socketio.on("message_event")
-def handle_message_event(json, methods=["GET","POST"]) -> dict:
+def handle_message_event(json, methods=["GET","POST"]):
     print(f"Event received: {str(json)}")
-    socketio.emit("my response", json, callback=message_confirm)
-
-    with open("chat_log.json", "a") as chat_log:
-        chat_log.write(str(json) + "\n")
+    for message in json:
+        chat = json[message]
+    socketio.emit("my response", chat, callback=message_confirm)
 
 def main() -> None:
     socketio.run(app, debug=True)
-
 
 if __name__ == '__main__':
     main()
