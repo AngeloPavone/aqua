cbf0e824cc05d42993f0536e7e3596a529249a90
AngeloPavone <pavone@gmx.us>
2023-02-11 11:59:06 -0800

    removed elements that should not appear before messege is sent

--- static/js/event_handler.js
+++ static/js/event_handler.js
@@ -1,13 +1,12 @@
 // Connect to socket.IO server
 const socket = io('http://127.0.0.1:5000')
 
-
 // Send the message to the server
 document.querySelector('form.chatbox').addEventListener('submit', function(e) {
-	e.preventDefault();
-	const message = document.getElementById('message-input').value;
-	socket.emit('send_message', message);
-	document.getElementById('message-input').value = '';
+    e.preventDefault();
+    const message = document.getElementById('message-input').value;
+    socket.emit('send_message', message);
+    document.getElementById('message-input').value = '';
 });
 
 // Receive a message from the server
@@ -16,11 +15,11 @@ socket.on('receive_message', function(data) {
 	const messageContainer = document.createElement('div');
 	messageContainer.classList.add('message-container');
 	messageContainer.classList.add(data.position);
-	
+
 	const messageBubble = document.createElement('div');
 	messageBubble.classList.add('message-bubble');
 	messageBubble.innerHTML = data.message;
-	
+
 	messageContainer.appendChild(messageBubble);
 	chatContainer.appendChild(messageContainer);
   });
